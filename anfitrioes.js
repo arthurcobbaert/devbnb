26265ee9f32fa961ffcd317751c42149e81946c0
Arthur Cobbaert <arthurcobbaert@Arthurs-MacBook-Air.local>
2024-09-25 19:22:22 -0300

    dados

---
+++ anfitrioes.js
@@ -0,0 +1,6 @@
+const anfitrioes = [
+    {
+        id_anfitriao: 1,
+        name_anf: 'Joao'
+    }
+]
---
+++ hospedagens.js
@@ -0,0 +1,22 @@
+const hospedagens = [
+    {
+        id_hos: 1,
+        name_hos: 'Gravatas',
+        tipo_hos: 'apartamento',
+        nota_hos: 3,
+        id_anfitriao: 1,
+        cidade_hos: 'Cabo Frio',
+        uf_hos: 'RJ'
+    },
+{
+
+        id_hos: 2,
+        name_hos: 'Corais e conchas',
+        tipo_hos: 'casa',
+        nota_hos: 4,
+        id_anfitriao: 1,
+        cidade_hos: 'Buzios',
+        uf_hos: 'RJ'
+},
+
+]
